99d966a0b234ebe87b98b108d32dd51be56bd889
allesharr <allesharr@gmail.com>
2021-01-16 14:30:28 +0300

    Обновление разбиения, добавлено отображение версии пакета. Возможен поиск по имени пакета либо его версии отдельно. Хранение имени и версии в списках, каждый i элемент списка соответствует имени и версии i пакета в Names( просто разделение для соответсвия базе и отсутсвия проблем при сравнении)

--- SystemSearch/src/Search.java
+++ SystemSearch/src/Search.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 
 public class Search {
@@ -9,35 +7,57 @@ public class Search {
         ArrayList<String> Names = new ArrayList<String>();
         Process p;
         try {
-            p = Runtime.getRuntime().exec("dpkg --get-selections");
+//            p = Runtime.getRuntime().exec("dpkg --get-selections");
+//            p = Runtime.getRuntime().exec("dpkg --get-selections");
+            p = Runtime.getRuntime().exec("dpkg-query -W");
             BufferedReader br = new BufferedReader(
                     new InputStreamReader(p.getInputStream()));
             while ((s = br.readLine()) != null)
-            Names.add(s);
+                Names.add(s);
             p.waitFor();
             p.destroy();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
 
         }
-        Names = DeleteInstallWord(Names);
-
-
-    }
-    public static ArrayList<String> DeleteInstallWord(ArrayList<String> Names)
-    {
+//        Names = DeleteInstallWord(Names);
         int Len = Names.size();
-        ArrayList<String> Result = new ArrayList<String>();
-        String s;
+        //Добавление разбиения на имя и версию. Нашел базу данных, где подобное разбиение по-умолчанию. https://security-tracker.debian.org/tracker/DSA-3638-1 - пример уязвимого пакета
+        ArrayList<String> Version = new ArrayList<String>();
+        ArrayList<String> OnlyName = new ArrayList<String>();
         for(int i = 0; i < Len; i++)
         {
             s = Names.get(i);
-            s = s.substring(0,s.indexOf("\t"));
-//            s.replace(" ","");
+            String version = s.substring(s.indexOf("\t"));
+            Version.add(version);
+            version = version.replace("\t","");
+            String onlyname = s.substring(0, s.indexOf("\t"));
+            OnlyName.add(onlyname);
             System.out.println(s);
-            Result.add(s);
+            System.out.println(onlyname);
+            System.out.println(version);
         }
-        return Result;
+        //проверка на ошибки
+
     }
+
+
+
+
+
+//    public static ArrayList<String> DeleteInstallWord(ArrayList<String> Names)
+//    {
+//        int Len = Names.size();
+//        ArrayList<String> Result = new ArrayList<String>();
+//        String s;
+//        for(int i = 0; i < Len; i++)
+//        {
+//            s = Names.get(i);
+//            s = s.substring(0,s.indexOf("\t"));
+////            s.replace(" ","");
+//            System.out.println(s);
+//            Result.add(s);
+//        }
+//        return Result;
+//    }
 
 }
