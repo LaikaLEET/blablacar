3ec2de4187ef272cdc6f301ce36b369001ac56b5
LaikaLEET <71174057+LaikaLEET@users.noreply.github.com>
2021-01-18 17:17:50 +0300

    Update Search.java
    Добавил кусок кода с парсингом html страницы.

--- SystemSearch/src/Search.java
+++ SystemSearch/src/Search.java
@@ -1,8 +1,30 @@
 import java.io.*;
 import java.util.ArrayList;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 public class Search {
     public static void main(String[] args) throws IOException, InterruptedException {
+        //Код для нахождения пакетов на сайте https://security-tracker.debian.org/tracker/data/json
+        FileWriter writer = new FileWriter("sources.txt", false);
+        String url = "https://security-tracker.debian.org/tracker/data/json";
+        Document doc = Jsoup.connect(url).ignoreContentType(true).userAgent("Mozilla").get();
+        String text = doc.body().text();
+        String result = "1";
+        text = text.substring(1);
+
+        for (String retval : text.split("}}}},")) {
+            result = (retval.split(":")[0]);
+            System.out.println(result);
+            writer.write(result + "\n");
+        }
+        FileReader reader = new FileReader("sources.txt");
+        int c;
+        while((c=reader.read())!=-1) {
+            System.out.print((char)c);
+        }
+        writer.close();
+        //Код Леши для выполнения команд в Linux
         String s;
         ArrayList<String> Names = new ArrayList<String>();
         Process p;
